15af38fdae6656bba07255b96a4b0d60bbb44903
Sender749 <jotmahal2003@gmail.com>
2025-03-24 20:34:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,16 +14,16 @@ def is_enabled(value, default):
         return default
 
 
-ADMIN = int(getenv("ADMIN", ""))
+ADMIN = int(getenv("ADMIN", "6541030917"))
 SILICON_PIC = os.environ.get("SILICON_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
-API_ID = int(getenv("API_ID", ""))
-API_HASH = str(getenv("API_HASH", ""))
+API_ID = int(getenv("API_ID", "25208597"))
+API_HASH = str(getenv("API_HASH", "e99c3c5693d6d23a143b6ce760b7a6de"))
 BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
-FORCE_SUB = os.environ.get("FORCE_SUB", "") 
-MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://replacewithyourmongodb:replacewithyourmongodb@cluster0.zi78j51.mongodb.net/?retryWrites=true&w=majority",))
+FORCE_SUB = os.environ.get("FORCE_SUB", "0") 
+MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://gd3251791:AGTtFa5C6lnTT1wC@cluster0.a7jt2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",))
 DEF_CAP = str(
     getenv(
         "DEF_CAP",
-        "<b>File Name:- `{file_name}`\n\n{file_size}</b>",
+        "<b>File Name:- `{file_name}`</b>",
     )
 )
